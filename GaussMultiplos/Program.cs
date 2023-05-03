9aa44a3c669562a0355140af2b797f8e07a2d957
Renan Lima <renanmslimabrasil@hotmail.com>
2023-05-03 13:23:51 -0300

    Adicionar arquivos de projeto.

---
+++ GaussMultiplos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+List<int> numbers = new List<int>();
+
+for(int i = 100; i > 0; i--)
+{
+    numbers.Add(i);
+}
+foreach(int count in numbers)
+{
+    Console.WriteLine(count % 5 == 0 ? "Renan" : count); 
+}
+Console.ReadLine();
